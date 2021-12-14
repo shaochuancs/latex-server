c390036238d122c1457ce927873a9212ad7fa646
shaochuan <shaochuan@360.cn>
2021-12-14 22:17:28 +0800

    LAT-42 improve maintainability

--- test/utils/network.test.ts
+++ test/utils/network.test.ts
@@ -26,7 +26,7 @@ describe('Test network utils', ()=>{
       statusCode: 200
     };
     const dateMock = new Date('2012-06-19T15:12:29Z');
-    expect(generateAccessRecord(reqMock, resMock, dateMock)).toBe('127.0.0.1 - - [19/Jun/2012:23:12:29 +0800] "GET /api/v1/info HTTP/1.1" 200 - "undefined" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"');
+    expect(generateAccessRecord(reqMock, resMock, dateMock)).toBe('127.0.0.1 - - [19/Jun/2012:15:12:29 +0000] "GET /api/v1/info HTTP/1.1" 200 - "undefined" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"');
   });
 
   test('Should parse port correctly', ()=>{
